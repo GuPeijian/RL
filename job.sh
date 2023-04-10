9004d9deaa92a6e32a75cc93e8d6e367fe9bfff0
GuPeijian <gpj123@mail.ustc.edu.cn>
2023-04-10 06:03:40 +0000

    job sh

--- job.sh
+++ job.sh
@@ -7,7 +7,8 @@ bash ./setup.sh
 echo "===============SETUP DONE=========="
 
 #bash ./run_train.sh
-bash ./run_eval.sh
+#bash ./run_eval.sh
+bash ./run_analysis.sh
 
 
 echo "===============JOB END============="
