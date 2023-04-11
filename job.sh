8a3e3c7bedc01b56238428e5d14affd85d9b15dc
GuPeijian <gpj123@mail.ustc.edu.cn>
2023-04-11 12:25:08 +0000

    hyper para

--- job.sh
+++ job.sh
@@ -8,6 +8,4 @@ echo "===============SETUP DONE=========="
 
 bash ./run_train.sh
 
-sleep 2h
-
 echo "===============JOB END============="
--- run_eval.sh
+++ run_eval.sh
@@ -11,11 +11,11 @@ python ./eval.py \
     --llm_dir ${LLM_DIR} \
     --seed ${SEED} \
     --max_length 1024 \
-    --num_train_epochs 10 \
+    --num_train_epochs 2 \
     --learning_rate 1e-4 \
-    --train_batch_size 32 \
+    --train_batch_size 16 \
     --eval_batch_size 32 \
-    --sample_num 4\
+    --sample_num 8\
     --warmup_ratio 0 \
     --weight_decay 0 \
     --output_dir ./output_rl
